8d41ebd0ff07d5e416dbdafb31f1e1cabf1ad01b
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-06-10 23:32:59 +0530

    first

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
-const fs = require('fs');
-const readline = require('readline');
+const fs = require("fs");
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-rl.question('Enter the filename: ', (filename) => {
-  rl.question('Enter the word to remove: ', (word) => {
+rl.question("Enter the filename: ", (filename) => {
+  rl.question("Enter the word to remove: ", (word) => {
     // TODO: Implement this function
+    fs.readFile(filename, "utf-8", (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      } else if (data) {
+        const modifiedData = data.replace(new RegExp(word, "g"), "");
+        fs.writeFile(filename, modifiedData, (err) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log("Success");
+        });
+      }
+    });
     rl.close();
   });
 });
